9530a4f55b0f77144c0f0951ec9120f98b329c4b
Song Wang <wangsong2012@gmail.com>
2020-03-09 00:33:20 -0700

    Update bert_tokenizer.py

--- tokenizing/bert_tokenizer.py
+++ tokenizing/bert_tokenizer.py
@@ -6,22 +6,27 @@ print(text)
 
 print("-------------------------")
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-tokenizer.add_special_tokens({"sep_token":'<S_SEP>'})
-print(len(tokenizer))
 
-print(tokenizer.tokenize(text))
+>>> tokenizer.tokenize(text)
+['the', 'only', 'thing', '<', 's', '_', 'sep', '>', 'people', 'are', 'actually', 'buying', 'it', '.']
 
+
+
+num_new_tokens = tokenizer.add_special_tokens({"sep_token":'<S_SEP>'})
+print(num_new_tokens)
+>>> print(len(tokenizer))
+30523
+>>> print(tokenizer.tokenize(text))
+['the', 'only', 'thing', '[UNK]', 'people', 'are', 'actually', 'buying', 'it', '.']
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-tokenizer.add_tokens(['<S_SEP>'])
+num_new_tokens = tokenizer.add_tokens(['<S_SEP>'])
+print(num_new_tokens)
 print(len(tokenizer))
-
+#30523
 print(tokenizer.tokenize(text))
-
-
-#The only thing  <S_SEP> People are actually buying it .
--------------------------
-#30523
-#['the', 'only', 'thing', '[UNK]', 'people', 'are', 'actually', 'buying', 'it', '.']
-#30523
 #['the', 'only', 'thing', '<s_sep>', 'people', 'are', 'actually', 'buying', 'it', '.']
+tokenizer.encode('<S_SEP>')
+#[101, 30522, 102]
+>>> tokenizer.decode([101,30522,102])
+'[CLS] <s_sep> [SEP]'
